5b97a19bf102c8f3c965b63a3f5bf3066bd7ccf3
hammadsaeed0 <hammadsaeed083@gmail.com>
2024-02-16 19:32:51 +0500

    Variables

---
+++ 01_basic/dataType.js
@@ -0,0 +1,15 @@
+// We Have Several Type of Data Type of JS:
+
+string => "HammadSaeed";
+number => 123425345;
+bigInt => 902837452938475920834759284357234;
+boolean => true or false;
+symbol => when we need unique value;
+null => standalne value mean empty;
+undefined => when we dont define the value of variable;
+
+// Not Premetive Type 
+
+Object 
+Array
+
