55135ca658a18e2b90727f999f5559e1a3777506
hammadsaeed0 <hammadsaeed083@gmail.com>
2024-02-18 19:44:35 +0500

    Number & Math

---
+++ 01_basic/number.js
@@ -0,0 +1,8 @@
+// console.log(Math.random());
+
+const min = 1000;
+const max = 9999;
+// console.log(max - min);
+// console.log(Math.floor(Math.random() * max - min) + min);
+
+//  Math Rondom Always provide value in between 0,1 and we devide that value to our desire number to get the acutual value like if we get 0.6 then we devide into 1000 to get 4 digit value but the problem is some time we also get 0 soo we add some value to handle this setuation and we use Math.floor to avoid all the decimal value
