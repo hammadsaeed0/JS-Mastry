45667c684e958a55f5f53cbe42bbe151f99e66ce
hammadsaeed0 <hammadsaeed083@gmail.com>
2024-02-18 15:44:15 +0500

    Strings

--- 01_basic/string.js
+++ 01_basic/string.js
@@ -1,4 +1,4 @@
-let isName = "Hammad";
+// let isName = "Hammad";
 
 // console.log(isName);
 
@@ -8,22 +8,54 @@ let isName = "Hammad";
 // Some usefull Method of String
 
 // console.log(isName[0]);
-
+// ----------------------------------------------------------------------------------------------------
 // console.log(isName.charAt(5));
+// const sentence = "The quick brown fox jumps over the lazy dog.";
+
+// const index = 4;
+
+// console.log(`The character at index ${index} is ${sentence.charAt(index)}`);
+// Expected output: "The character at index 4 is q"
+// ---------------------------------------------------------------------------------------------------
 // console.log(isName.concat("saeed", "khan"));
-// console.log(isName.endsWith("k"));
+// console.log(isName.endsWith("d"));
 // console.log(isName.includes("m"));
 // const sentence = "The quick brown fox jumps over the lazy dog.";
 
 // const word = "the";
 
-// console.log(sentence.includes(word));
+// console.log(isName.includes("m"));
 // // Expected output: "The word "fox" is in the sentence"
 
 // console.log(isName.indexOf("m"));
 // console.log(isName.repeat(100));
 // console.log(isName.replace("Ham", "saeed"));
 // console.log(isName.search("d"));
-// console.log(isName.slice(1, 2));
 
-// console.log(isName.length);
+// const str = "The quick brown fox jumps over the lazy dog.";
+
+// // console.log(str.slice(31));
+// // // Expected output: "the lazy dog."
+
+// // console.log(str.slice(4, 19));
+// // // Expected output: "quick brown fox"
+
+// // console.log(isName.slice(-2, -1));
+// // // Expected output: "dog."
+
+// console.log(str.slice(-9, -8));
+// // // Expected output: "lazy"
+
+// const str = "The quick brown fox jumps over the lazy dog.";
+
+// const words = str.split(" ");
+// console.log(words);
+// // Expected output: "fox"
+
+// const chars = str.split("");
+// console.log(chars);
+// Expected output: "k"
+
+// const strCopy = str.split();
+// console.log(strCopy);
+// // Expected output: Array ["The quick brown fox jumps over the lazy dog."]
